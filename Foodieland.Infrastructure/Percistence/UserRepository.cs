e2e8a76d3ab6bcd20290b8bcacdc74c6c7f21ddb
skmkqw <tkorsakov77@gmail.com>
2024-09-03 21:58:45 +0200

    UserRepository

---
+++ Foodieland.Infrastructure/Percistence/UserRepository.cs
@@ -0,0 +1,18 @@
+using Foodieland.Application.Common.Interfaces.Persistence;
+using Foodieland.Domain.Entities;
+
+namespace Foodieland.Infrastructure.Percistence;
+
+public class UserRepository : IUserRepository
+{
+    private static readonly List<User> _users = new();
+    public User? GetUserByEmail(string email)
+    {
+        return _users.FirstOrDefault(u => u.Email == email);
+    }
+
+    public void AddUser(User user)
+    {
+        _users.Add(user);
+    }
+}
