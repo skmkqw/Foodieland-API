fe1d49055d51abfd63355d60c857caa3abc5239d
skmkqw <tkorsakov77@gmail.com>
2024-09-03 21:59:53 +0200

    using UserRepository

--- Foodieland.Application/Services/Authentication/AuthenticationService.cs
+++ Foodieland.Application/Services/Authentication/AuthenticationService.cs
@@ -1,25 +1,50 @@
 using Foodieland.Application.Common.Interfaces;
+using Foodieland.Application.Common.Interfaces.Authentication;
+using Foodieland.Application.Common.Interfaces.Persistence;
+using Foodieland.Domain.Entities;
 
 namespace Foodieland.Application.Services.Authentication;
 
 public class AuthenticationService : IAuthenticationService
 {
     private readonly IJwtTokenGenerator _jwtTokenGenerator;
+    private readonly IUserRepository _userRepository;
 
-    public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+    public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator, IUserRepository userRepository)
     {
         _jwtTokenGenerator = jwtTokenGenerator;
+        _userRepository = userRepository;
     }
 
     public AuthenticationResult Register(string firstName, string lastName, string email, string password)
     {
-        Guid userId = Guid.NewGuid();
-        var token = _jwtTokenGenerator.GenerateToken(userId, firstName, lastName);
-        return new AuthenticationResult(Guid.NewGuid(), firstName, lastName, email, token);
+        if (_userRepository.GetUserByEmail(email) != null)
+            throw new Exception("User with given email already exists");
+
+        User user = new User
+        {
+            Email = email,
+            FirstName = firstName, 
+            LastName = lastName,
+            Password = password
+        };
+        
+        _userRepository.AddUser(user);
+        
+        var token = _jwtTokenGenerator.GenerateToken(user);
+        return new AuthenticationResult(user, token);
     }
 
     public AuthenticationResult Login(string email, string password)
     {
-        return new AuthenticationResult(Guid.NewGuid(), "firstName", "lastName", email, "token");
+        if (_userRepository.GetUserByEmail(email) is not User user)
+            throw new Exception("User with given email does not exist");
+        
+        if (user.Password != password)
+            throw new Exception("Passwords do not match");
+        
+        var token = _jwtTokenGenerator.GenerateToken(user);
+        
+        return new AuthenticationResult(user, token);
     }
 }
