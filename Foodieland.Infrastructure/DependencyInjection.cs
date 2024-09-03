e8bee8b18e836439cb3b39228d452a07d445065a
skmkqw <tkorsakov77@gmail.com>
2024-09-03 19:09:58 +0200

    registered JwtTokenGenerator as Singleton

--- Foodieland.Infrastructure/DependencyInjection.cs
+++ Foodieland.Infrastructure/DependencyInjection.cs
@@ -8,7 +8,7 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
-        services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
+        services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
         return services;
     }
 }
