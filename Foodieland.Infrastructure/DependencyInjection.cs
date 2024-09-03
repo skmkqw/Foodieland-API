9ae3b7a3e7cf77b5636199e293878e39373e6802
skmkqw <tkorsakov77@gmail.com>
2024-09-03 20:13:18 +0200

    jwtsettings DI

--- Foodieland.Infrastructure/DependencyInjection.cs
+++ Foodieland.Infrastructure/DependencyInjection.cs
@@ -2,14 +2,16 @@ using Foodieland.Application.Common.Interfaces;
 using Foodieland.Application.Common.Services;
 using Foodieland.Infrastructure.Authentication;
 using Foodieland.Infrastructure.Services;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Foodieland.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, ConfigurationManager configuration)
     {
+        services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
         services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
         services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
         return services;
