59e997afb368fc8cb39d250c7b05b9e2215dfff4
skmkqw <tkorsakov77@gmail.com>
2024-09-03 17:46:05 +0200

    mock register endpoint

--- Foodieland.API/Controllers/AuthenticationController.cs
+++ Foodieland.API/Controllers/AuthenticationController.cs
@@ -1,3 +1,4 @@
+using Foodieland.Contracts.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using LoginRequest = Foodieland.Contracts.Authentication.LoginRequest;
 
@@ -7,6 +8,12 @@ namespace Foodieland.API.Controllers;
 [Route("auth")]
 public class AuthenticationController : ControllerBase
 {
+    [HttpPost("register")]
+    public IActionResult Register(RegisterRequest request)
+    {
+        return Ok(request);
+    }
+    
     [HttpPost("login")]
     public IActionResult Login(LoginRequest request)
     {
