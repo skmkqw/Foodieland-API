9cdddd299d608def3426c34cbeefe482d8b02f03
skmkqw <tkorsakov77@gmail.com>
2024-09-03 20:13:32 +0200

    using jwtSettings values in JwtTokenGenerator

--- Foodieland.Infrastructure/Authentication/JwtTokenGenerator.cs
+++ Foodieland.Infrastructure/Authentication/JwtTokenGenerator.cs
@@ -3,6 +3,7 @@ using System.Security.Claims;
 using System.Text;
 using Foodieland.Application.Common.Interfaces;
 using Foodieland.Application.Common.Services;
+using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 
 namespace Foodieland.Infrastructure.Authentication;
@@ -10,17 +11,20 @@ namespace Foodieland.Infrastructure.Authentication;
 public class JwtTokenGenerator : IJwtTokenGenerator
 {
     private readonly IDateTimeProvider _dateTimeProvider;
+    
+    private readonly JwtSettings _jwtSettings;
 
-    public JwtTokenGenerator(IDateTimeProvider dateTimeProvider)
+    public JwtTokenGenerator(IDateTimeProvider dateTimeProvider, IOptions<JwtSettings> jwtSettings)
     {
         _dateTimeProvider = dateTimeProvider;
+        _jwtSettings = jwtSettings.Value;
     }
 
     public string GenerateToken(Guid userId, string firstName, string lastName)
     {
         var signingCredentials = new SigningCredentials(
             new SymmetricSecurityKey(
-                Encoding.UTF8.GetBytes("super-secret-keysuper-secret-key")), 
+                Encoding.UTF8.GetBytes(_jwtSettings.Secret)), 
             SecurityAlgorithms.HmacSha256);
         
         var claims = new[]
@@ -34,8 +38,9 @@ public class JwtTokenGenerator : IJwtTokenGenerator
         var securityToken = new JwtSecurityToken(
             claims: claims, 
             signingCredentials: signingCredentials, 
-            issuer: "Foodieland",
-            expires: _dateTimeProvider.UtcNow.AddDays(1));
+            issuer: _jwtSettings.Issuer,
+            audience: _jwtSettings.Audience,
+            expires: _dateTimeProvider.UtcNow.AddDays(_jwtSettings.ExpiryDays));
         
         return new JwtSecurityTokenHandler().WriteToken(securityToken);
     }
