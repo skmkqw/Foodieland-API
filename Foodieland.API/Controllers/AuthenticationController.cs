bbe7541f02875c0969cacdb1dd4fea30838ac880
skmkqw <tkorsakov77@gmail.com>
2024-09-03 18:04:10 +0200

    using AuthenticationService in AuthenticationController

--- Foodieland.API/Controllers/AuthenticationController.cs
+++ Foodieland.API/Controllers/AuthenticationController.cs
@@ -1,3 +1,4 @@
+using Foodieland.Application.Services.Authentication;
 using Foodieland.Contracts.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using LoginRequest = Foodieland.Contracts.Authentication.LoginRequest;
@@ -8,15 +9,46 @@ namespace Foodieland.API.Controllers;
 [Route("auth")]
 public class AuthenticationController : ControllerBase
 {
+    private readonly IAuthenticationService _authenticationService;
+
+    public AuthenticationController(IAuthenticationService authenticationService)
+    {
+        _authenticationService = authenticationService;
+    }
+
     [HttpPost("register")]
     public IActionResult Register(RegisterRequest request)
     {
-        return Ok(request);
+        var authResult = _authenticationService.Register(
+            request.FirstName, 
+            request.LastName, 
+            request.Email, 
+            request.Password);
+
+        var response = new AuthenticationResponse(
+            authResult.Id,
+            authResult.FirstName,
+            authResult.LastName,
+            authResult.Email,
+            authResult.Token);
+        
+        return Ok(response);
     }
     
     [HttpPost("login")]
     public IActionResult Login(LoginRequest request)
     {
-        return Ok(request);
+        var authResult = _authenticationService.Login(
+            request.Email, 
+            request.Password);
+
+        var response = new AuthenticationResponse(
+            authResult.Id,
+            authResult.FirstName,
+            authResult.LastName,
+            authResult.Email,
+            authResult.Token);
+        
+        return Ok(response);
     }
 }
