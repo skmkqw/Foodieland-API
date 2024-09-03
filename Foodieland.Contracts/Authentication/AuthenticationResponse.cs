2b925ae801d1b36a7eb88eb97397684e4484774a
skmkqw <tkorsakov77@gmail.com>
2024-09-03 17:27:55 +0200

    AuthenticationResponse

---
+++ Foodieland.Contracts/Authentication/AuthenticationResponse.cs
@@ -0,0 +1,8 @@
+namespace Foodieland.Contracts.Authentication;
+
+public record AuthenticationResponse(
+    Guid Id,
+    string FirstName, 
+    string LastName, 
+    string Email, 
+    string Token);
