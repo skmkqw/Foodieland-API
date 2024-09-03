e4512899505eb3f9148d20f492dce311a36d9fb3
skmkqw <tkorsakov77@gmail.com>
2024-09-03 18:16:17 +0200

    AddApplication extension method

---
+++ Foodieland.Application/DependencyInjection.cs
@@ -0,0 +1,13 @@
+using Foodieland.Application.Services.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Foodieland.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddScoped<IAuthenticationService, AuthenticationService>();
+        return services;
+    }
+}
