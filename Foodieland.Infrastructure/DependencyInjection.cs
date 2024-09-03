47a221db1a90f862cebecb1f8b9a6cbc02c217dc
skmkqw <tkorsakov77@gmail.com>
2024-09-03 19:15:54 +0200

    registered DateTimeProvider in AddInfrastructure

--- Foodieland.Infrastructure/DependencyInjection.cs
+++ Foodieland.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,7 @@
 using Foodieland.Application.Common.Interfaces;
+using Foodieland.Application.Common.Services;
 using Foodieland.Infrastructure.Authentication;
+using Foodieland.Infrastructure.Services;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Foodieland.Infrastructure;
@@ -9,6 +11,7 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
         services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+        services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
         return services;
     }
 }
