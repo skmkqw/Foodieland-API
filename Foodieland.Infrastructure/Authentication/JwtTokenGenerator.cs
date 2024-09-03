9add6d32f3a6f6ef00fce683b6841bdd916716c0
skmkqw <tkorsakov77@gmail.com>
2024-09-03 18:51:16 +0200

    JwtTokenGenerator

---
+++ Foodieland.Infrastructure/Authentication/JwtTokenGenerator.cs
@@ -0,0 +1,33 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Foodieland.Application.Common.Interfaces;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Foodieland.Infrastructure.Authentication;
+
+public class JwtTokenGenerator : IJwtTokenGenerator
+{
+    public string GenerateToken(Guid userId, string firstName, string lastName)
+    {
+        var signingCredentials = new SigningCredentials(
+            new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes("super-secret-key")), 
+            SecurityAlgorithms.HmacSha256);
+        
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.GivenName, firstName),
+            new Claim(ClaimTypes.Surname, lastName),
+            new Claim(ClaimTypes.NameIdentifier, userId.ToString()),
+        };
+
+        var securityToken = new JwtSecurityToken(
+            claims: claims, 
+            signingCredentials: signingCredentials, 
+            issuer: "Foodieland",
+            expires: DateTime.Now.AddDays(1));
+        
+        return new JwtSecurityTokenHandler().WriteToken(securityToken);
+    }
+}
