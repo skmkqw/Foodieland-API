4d047550bf796893f9e2449ece457b6844068065
skmkqw <tkorsakov77@gmail.com>
2024-09-04 21:10:54 +0200

    removed Services directory

--- Foodieland.Application/Services/Authentication/Queries/IAuthenticationQueryService.cs
+++
@@ -1,9 +0,0 @@
-using ErrorOr;
-using Foodieland.Application.Services.Authentication.Common;
-
-namespace Foodieland.Application.Services.Authentication.Queries;
-
-public interface IAuthenticationQueryService
-{
-    ErrorOr<AuthenticationResult> Login(string email, string password);
-}
