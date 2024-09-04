cd204b59daea13c33580c4191f3742a3b68f7ce0
skmkqw <tkorsakov77@gmail.com>
2024-09-04 20:11:50 +0200

    updated DependencyInjection

--- Foodieland.Application/DependencyInjection.cs
+++ Foodieland.Application/DependencyInjection.cs
@@ -1,4 +1,6 @@
 using Foodieland.Application.Services.Authentication;
+using Foodieland.Application.Services.Authentication.Commands;
+using Foodieland.Application.Services.Authentication.Queries;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Foodieland.Application;
@@ -7,7 +9,8 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        services.AddScoped<IAuthenticationService, AuthenticationService>();
+        services.AddScoped<IAuthenticationCommandService, AuthenticationCommandService>();
+        services.AddScoped<IAuthenticationQueryService, AuthenticationQueryService>();
         return services;
     }
 }
