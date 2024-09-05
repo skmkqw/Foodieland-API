6c4be0c8ce032ae62e56d273d9e1232c9eee091b
skmkqw <tkorsakov77@gmail.com>
2024-09-05 16:44:58 +0200

    updated ValidationBehavior

--- Foodieland.Application/Common/Behaviors/ValidationBehavior.cs
+++ Foodieland.Application/Common/Behaviors/ValidationBehavior.cs
@@ -1,4 +1,5 @@
 using ErrorOr;
+using FluentValidation;
 using Foodieland.Application.Authentication.Commands.Register;
 using Foodieland.Application.Authentication.Common;
 using MediatR;
@@ -7,12 +8,29 @@ namespace Foodieland.Application.Common.Behaviors;
 
 public class ValidateRegisterCommandBehavior : IPipelineBehavior<RegisterCommand, ErrorOr<AuthenticationResult>>
 {
+    private readonly IValidator<RegisterCommand> _validator;
+
+    public ValidateRegisterCommandBehavior(IValidator<RegisterCommand> validator)
+    {
+        _validator = validator;
+    }
+
     public async Task<ErrorOr<AuthenticationResult>> Handle(
         RegisterCommand request, 
         RequestHandlerDelegate<ErrorOr<AuthenticationResult>> next, 
         CancellationToken cancellationToken)
     {
-        var result = await next();
-        return result;
+        var validationResult = await _validator.ValidateAsync(request);
+
+        if (validationResult.IsValid)
+        {
+            return await next();
+        }
+        
+        var errors = validationResult.Errors
+            .Select(e => Error.Validation(e.PropertyName, e.ErrorMessage))
+            .ToList();
+        
+        return errors;
     }
 }
