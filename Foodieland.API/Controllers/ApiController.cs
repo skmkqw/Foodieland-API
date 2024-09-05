57d57516a46d026e6d8d9f7144b27a5722e73ed7
skmkqw <tkorsakov77@gmail.com>
2024-09-05 17:13:39 +0200

    refactored ApiController

--- Foodieland.API/Controllers/ApiController.cs
+++ Foodieland.API/Controllers/ApiController.cs
@@ -1,5 +1,6 @@
 using ErrorOr;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace Foodieland.API.Controllers;
 
@@ -8,8 +9,17 @@ public class ApiController : ControllerBase
 {
     protected IActionResult Problem(List<Error> errors)
     {
-        var firstError = errors.First();
+        if (errors.Count == 0)
+            return Problem();
         
+        if (errors.All(error => error.Type == ErrorType.Validation))
+            return ValidationProblem(errors);
+        
+        return Problem(errors.First());
+    }
+
+    private IActionResult Problem(Error firstError)
+    {
         var statusCode = firstError.Type switch
         {
             ErrorType.Conflict => StatusCodes.Status409Conflict,
@@ -18,7 +28,17 @@ public class ApiController : ControllerBase
             ErrorType.Validation => StatusCodes.Status400BadRequest,
             _ => StatusCodes.Status500InternalServerError
         };
-        
+
         return Problem(statusCode: statusCode, title: firstError.Description);
+    }
+
+    private IActionResult ValidationProblem(List<Error> errors)
+    {
+        var modelStateDictionary = new ModelStateDictionary();
+        foreach (var error in errors)
+        {
+            modelStateDictionary.AddModelError(error.Code, error.Description);
+        }
+        return ValidationProblem(modelStateDictionary);
     }
 }
