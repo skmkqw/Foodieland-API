43bae94d08a6e5eee10ad1f330055ead94df9aea
skmkqw <tkorsakov77@gmail.com>
2024-09-06 23:59:14 +0200

    Ingredient as entity

---
+++ Foodieland.Domain/RecipeAggregate/Entities/Ingredient.cs
@@ -0,0 +1,25 @@
+using Foodieland.Domain.Common.Models;
+using Foodieland.Domain.RecipeAggregate.ValueObjects;
+
+namespace Foodieland.Domain.RecipeAggregate.Entities;
+
+public class Ingredient : Entity<IngredientId>
+{
+    public string Name { get; }
+    
+    public float Quantity { get; }
+    
+    public string Unit { get; }
+    
+    private Ingredient(IngredientId id, string name, float quantity, string unit) : base(id)
+    {
+        Name = name;
+        Quantity = quantity;
+        Unit = unit;
+    }
+
+    public static Ingredient Create(IngredientId id, string name, float quantity, string unit)
+    {
+        return new Ingredient(IngredientId.CreateUnique(), name, quantity, unit);
+    }
+}
