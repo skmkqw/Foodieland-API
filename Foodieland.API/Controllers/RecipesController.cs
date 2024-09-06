b041351ead7fcf51b46b93251530fe1e15eb0403
skmkqw <tkorsakov77@gmail.com>
2024-09-07 00:53:44 +0200

    updated CreateRecipe endpoint

--- Foodieland.API/Controllers/RecipesController.cs
+++ Foodieland.API/Controllers/RecipesController.cs
@@ -1,4 +1,7 @@
+using Foodieland.Application.Recipes.Commands.CreateRecipe;
 using Foodieland.Contracts.Recipes;
+using MapsterMapper;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Foodieland.API.Controllers;
@@ -6,9 +9,22 @@ namespace Foodieland.API.Controllers;
 [Route("[controller]")]
 public class RecipesController : ApiController
 {
-    [HttpPost]
-    public IActionResult CreateRecipe(CreateRecipeRequest request)
+    private readonly IMapper _mapper;
+    
+    private readonly IMediator _mediator;
+
+    public RecipesController(IMapper mapper, IMediator mediator)
     {
+        _mapper = mapper;
+        _mediator = mediator;
+    }
+
+    [HttpPost("/recipes")]
+    public async Task<IActionResult> CreateRecipe(CreateRecipeRequest request)
+    {
+        var command = _mapper.Map<CreateRecipeCommand>((request, "a"));
+        
+        var createRecipeResult = await _mediator.Send(command);
         return Ok(request);
     }
 }
