9db1a99aa842f12dcc14bccc0344b91731bbcf19
skmkqw <tkorsakov77@gmail.com>
2024-09-06 22:14:13 +0200

    updated Recipe

--- Foodieland.Domain/Recipe/Recipe.cs
+++ Foodieland.Domain/Recipe/Recipe.cs
@@ -2,6 +2,7 @@ using Foodieland.Domain.Common.Models;
 using Foodieland.Domain.Ingredient.ValueObjects;
 using Foodieland.Domain.Recipe.Entities;
 using Foodieland.Domain.Recipe.ValueObjects;
+using Foodieland.Domain.Review.ValueObjects;
 using Foodieland.Domain.User.ValueObjects;
 
 namespace Foodieland.Domain.Recipe;
@@ -21,12 +22,16 @@ public sealed class Recipe : AggregateRoot<RecipeId>
     public NutritionInformation? NutritionInformation => _nutritionInformation;
 
     public IReadOnlyList<IngredientId> IngredientsIds => _ingredientIds.AsReadOnly();
+    
+    public IReadOnlyList<ReviewId> ReviewIds => _reviewIds.AsReadOnly();
 
     private NutritionInformation? _nutritionInformation;
 
     private readonly List<CookingDirection> _directions = new();
     
     private readonly List<IngredientId> _ingredientIds = new();
+    
+    private readonly List<ReviewId> _reviewIds = new();
     
     private Recipe(RecipeId id, string name, string description, int timeToCook, UserId creatorId) : base(id)
     {
