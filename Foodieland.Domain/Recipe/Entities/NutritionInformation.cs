3271a774ef6b0765f694c5f0912f6cf1e4a0c491
skmkqw <tkorsakov77@gmail.com>
2024-09-06 21:42:16 +0200

    NutritionInformation

---
+++ Foodieland.Domain/Recipe/Entities/NutritionInformation.cs
@@ -0,0 +1,28 @@
+using Foodieland.Domain.Common.Models;
+using Foodieland.Domain.Recipe.ValueObjects;
+
+namespace Foodieland.Domain.Recipe.Entities;
+
+public sealed class NutritionInformation : Entity<NutritionInformationId>
+{
+    public int Calories { get; }
+    
+    public float Fat { get; }
+    
+    public float Carbohydrates { get; }
+    
+    public float Protein { get; }
+
+    private NutritionInformation(NutritionInformationId id, int calories, float fat, float carbohydrates, float protein) : base(id)
+    {
+        Calories = calories;
+        Fat = fat;
+        Carbohydrates = carbohydrates;
+        Protein = protein;
+    }
+
+    public static NutritionInformation CreateNew(int calories, float fat, float carbohydrates, float protein)
+    {
+        return new NutritionInformation(NutritionInformationId.CreateUnique(), calories, fat, carbohydrates, protein);
+    }
+}
