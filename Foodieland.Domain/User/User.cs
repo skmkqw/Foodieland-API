1e41713978fd9c939dd67cffe7f3651df6b0aa4d
skmkqw <tkorsakov77@gmail.com>
2024-09-06 22:06:24 +0200

    User aggregate

---
+++ Foodieland.Domain/User/User.cs
@@ -0,0 +1,28 @@
+using Foodieland.Domain.Common.Models;
+using Foodieland.Domain.User.ValueObjects;
+
+namespace Foodieland.Domain.User;
+
+public sealed class User : AggregateRoot<UserId>
+{
+    public string FirstName { get; }
+
+    public string LastName { get; }
+
+    public string Email { get; }
+
+    public string Password { get; }
+    
+    private User(UserId id, string firstName, string lastName, string email, string password) : base(id)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        Password = password;
+    }
+
+    public static User Create(string firstName, string lastName, string email, string password)
+    {
+        return new User(UserId.CreateUnique(), firstName, lastName, email, password);
+    }
+}
