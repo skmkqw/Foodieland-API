2a83a8b432c0634e4de8aac8f1fdc4a7707becf1
skmkqw <tkorsakov77@gmail.com>
2024-09-07 00:53:31 +0200

    RecipeMappingConfiguration

---
+++ Foodieland.API/Mapping/RecipeMappingConfiguration.cs
@@ -0,0 +1,15 @@
+using Foodieland.Application.Recipes.Commands.CreateRecipe;
+using Foodieland.Contracts.Recipes;
+using Mapster;
+
+namespace Foodieland.API.Mapping;
+
+public class RecipeMappingConfiguration : IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        config.NewConfig<(CreateRecipeRequest request, string creatorId), CreateRecipeCommand>()
+            .Map(dest => dest.CreatorId, src => src.creatorId)
+            .Map(dest => dest, src => src.request);
+    }
+}
