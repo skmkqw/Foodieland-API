c8df920924aa8a2edeca609ea40d0d4b8831144e
skmkqw <tkorsakov77@gmail.com>
2024-09-06 21:45:20 +0200

    IngredientId

---
+++ Foodieland.Domain/Ingredient/ValueObjects/IngredientId.cs
@@ -0,0 +1,23 @@
+using Foodieland.Domain.Common.Models;
+
+namespace Foodieland.Domain.Ingredient.ValueObjects;
+
+public class IngredientId : ValueObject
+{
+    public Guid Value { get; }
+
+    private IngredientId(Guid value)
+    {
+        Value = value;
+    }
+
+    public static IngredientId CreateUnique()
+    {
+        return new IngredientId(Guid.NewGuid());
+    }
+    
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
