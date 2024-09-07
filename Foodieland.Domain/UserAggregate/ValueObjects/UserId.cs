a1514874e02ae067d3c99245e74c3d3f4811eb6f
skmkqw <tkorsakov77@gmail.com>
2024-09-07 17:06:43 +0200

    UserId.Create method

--- Foodieland.Domain/UserAggregate/ValueObjects/UserId.cs
+++ Foodieland.Domain/UserAggregate/ValueObjects/UserId.cs
@@ -10,10 +10,15 @@ public class UserId : ValueObject
     {
         Value = value;
     }
-
+    
     public static UserId CreateUnique()
     {
         return new UserId(Guid.NewGuid());
+    }
+
+    public static UserId Create(Guid value)
+    {
+        return new UserId(value);
     }
     
     protected override IEnumerable<object> GetEqualityComponents()
