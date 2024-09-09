78ed063037e23a1307c36144c20cdda4b330c017
skmkqw <tkorsakov77@gmail.com>
2024-09-09 16:28:07 +0200

    updated Entity class

--- Foodieland.Domain/Common/Models/Entity.cs
+++ Foodieland.Domain/Common/Models/Entity.cs
@@ -1,13 +1,26 @@
 namespace Foodieland.Domain.Common.Models;
 
-public abstract class Entity<TId> : IEquatable<Entity<TId>>
+public abstract class Entity<TId> : IEquatable<Entity<TId>>, IHasDomainEvents
     where TId : notnull
 {
+    private readonly List<IDomainEvent> _domainEvents = new();
     public TId Id { get; protected set; }
+    
+    public IReadOnlyList<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+    
+    public void ClearDomainEvents()
+    {
+        _domainEvents.Clear();
+    }
 
     protected Entity(TId id)
     {
         Id = id;
+    }
+    
+    public void AddDomainEvent(IDomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
     }
     
     public override bool Equals(object? obj)
