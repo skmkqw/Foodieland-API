2ba641eb34ccca2406b90e392574555912ab33c4
skmkqw <tkorsakov77@gmail.com>
2024-09-09 16:37:52 +0200

    Dummy event handler

---
+++ Foodieland.Application/Recipes/Events/DummyHandler.cs
@@ -0,0 +1,12 @@
+using Foodieland.Domain.RecipeAggregate.Events;
+using MediatR;
+
+namespace Foodieland.Application.Recipes.Events;
+
+public class DummyHandler : INotificationHandler<RecipeCreated>
+{
+    public Task Handle(RecipeCreated notification, CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
