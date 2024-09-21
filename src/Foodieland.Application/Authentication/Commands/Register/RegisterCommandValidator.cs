05624ea06b895f9b3b7f1f970b70c8aee2a4db38
skmkqw <tkorsakov77@gmail.com>
2024-09-21 18:06:29 +0200

    new validation rules for RegisterCommand

--- src/Foodieland.Application/Authentication/Commands/Register/RegisterCommandValidator.cs
+++ src/Foodieland.Application/Authentication/Commands/Register/RegisterCommandValidator.cs
@@ -6,9 +6,20 @@ public class RegisterCommandValidator : AbstractValidator<RegisterCommand>
 {
     public RegisterCommandValidator()
     {
-        RuleFor(x => x.Email).NotEmpty().WithMessage("Email is required");
-        RuleFor(x => x.Password).NotEmpty().WithMessage("Password is required");
-        RuleFor(x => x.FirstName).NotEmpty().WithMessage("First name is required");
-        RuleFor(x => x.LastName).NotEmpty().WithMessage("Last name is required");
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required");
+        
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required")
+            .MinimumLength(8).WithMessage("Password must be at least 8 characters long")
+            .Matches(@"[A-Z]").WithMessage("Password must contain at least one uppercase letter")
+            .Matches(@"[a-z]").WithMessage("Password must contain at least one lowercase letter")
+            .Matches(@"\d").WithMessage("Password must contain at least one number")
+            .Matches(@"[\W]").WithMessage("Password must contain at least one special character");
+        
+        RuleFor(x => x.FirstName)
+            .NotEmpty().WithMessage("First name is required");
+        RuleFor(x => x.LastName)
+            .NotEmpty().WithMessage("Last name is required");
     }
 }
