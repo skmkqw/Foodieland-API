201e1e1bd067884597d79c79bba9ff5581a9b10d
skmkqw <tkorsakov77@gmail.com>
2024-09-22 21:43:14 +0200

    DeleteRecipeCommandHandler

---
+++ src/Foodieland.Application/Recipes/Commands/DeleteRecipe/DeleteRecipeCommandHandler.cs
@@ -0,0 +1,36 @@
+using ErrorOr;
+using Foodieland.Application.Common.Interfaces.Persistence;
+using MediatR;
+
+namespace Foodieland.Application.Recipes.Commands.DeleteRecipe;
+
+public class DeleteRecipeCommandHandler : IRequestHandler<DeleteRecipeCommand, ErrorOr<Unit>>
+{
+    private readonly IRecipeRepository _recipeRepository;
+    
+    public DeleteRecipeCommandHandler(IRecipeRepository recipeRepository)
+    {
+        _recipeRepository = recipeRepository;
+    }
+
+    public async Task<ErrorOr<Unit>> Handle(DeleteRecipeCommand request, CancellationToken cancellationToken)
+    {
+        await Task.CompletedTask;
+        
+        var recipe = _recipeRepository.GetRecipeById(request.RecipeId);
+
+        if (recipe is null)
+        { 
+            return Error.NotFound("Recipe.NotFound", "Recipe not found or don't exist");
+        }
+
+        if (recipe.CreatorId != request.CreatorId)
+        {
+            return Error.Unauthorized("Recipe.Unauthorized", "You are not authorized to delete this recipe.");
+        }
+        
+        _recipeRepository.DeleteRecipe(recipe);
+
+        return Unit.Value;
+    }
+}
