273cfed17d3d1432c1c8e8a23c880ae1bd464d14
skmkqw <tkorsakov77@gmail.com>
2024-09-22 20:59:11 +0200

    actual RecipeCreated event handling

--- src/Foodieland.Application/Recipes/Events/RecipeCreatedEventHandler.cs
+++ src/Foodieland.Application/Recipes/Events/RecipeCreatedEventHandler.cs
@@ -1,3 +1,4 @@
+using Foodieland.Application.Common.Interfaces.Persistence;
 using Foodieland.Domain.RecipeAggregate.Events;
 using MediatR;
 
@@ -5,8 +6,19 @@ namespace Foodieland.Application.Recipes.Events;
 
 public class RecipeCreatedEventHandler : INotificationHandler<RecipeCreated>
 {
+    private readonly IUserRepository _userRepository;
+
+    public RecipeCreatedEventHandler(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+    
     public Task Handle(RecipeCreated notification, CancellationToken cancellationToken)
     {
+        var recipeCreator = _userRepository.GetUserById(notification.Recipe.CreatorId);
+        
+        recipeCreator!.AddRecipe(notification.Recipe.Id);
+        
         return Task.CompletedTask;
     }
 }
