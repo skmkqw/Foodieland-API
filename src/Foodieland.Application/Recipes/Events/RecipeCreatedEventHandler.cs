2a9ba72da42b8f9ab68506a3ac727727293577c5
skmkqw <tkorsakov77@gmail.com>
2024-09-22 20:50:19 +0200

    renamed DummyHandler into RecipeCreatedEventHandler

--- src/Foodieland.Application/Recipes/Events/RecipeCreatedEventHandler.cs
+++ src/Foodieland.Application/Recipes/Events/RecipeCreatedEventHandler.cs
@@ -1,10 +1,9 @@
-using Foodieland.Application.Common.Interfaces.Persistence;
 using Foodieland.Domain.RecipeAggregate.Events;
 using MediatR;
 
 namespace Foodieland.Application.Recipes.Events;
 
-public class DummyHandler : INotificationHandler<RecipeCreated>
+public class RecipeCreatedEventHandler : INotificationHandler<RecipeCreated>
 {
     public Task Handle(RecipeCreated notification, CancellationToken cancellationToken)
     {
