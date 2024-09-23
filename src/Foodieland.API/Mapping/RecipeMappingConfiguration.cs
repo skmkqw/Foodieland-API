fa65573132b513595a1367192831c07e53f216a8
skmkqw <tkorsakov77@gmail.com>
2024-09-23 20:31:28 +0200

    updated RecipeMappingConfiguration

--- src/Foodieland.API/Mapping/RecipeMappingConfiguration.cs
+++ src/Foodieland.API/Mapping/RecipeMappingConfiguration.cs
@@ -1,6 +1,7 @@
 using Foodieland.Application.Recipes.Commands.CreateRecipe;
 using Foodieland.Application.Recipes.Commands.DeleteRecipe;
-using Foodieland.Contracts.Recipes;
+using Foodieland.Contracts.Recipes.Common;
+using Foodieland.Contracts.Recipes.CreateRecipe;
 using Foodieland.Domain.RecipeAggregate;
 using Foodieland.Domain.RecipeAggregate.ValueObjects;
 using Foodieland.Domain.UserAggregate.ValueObjects;
@@ -23,13 +24,17 @@ public class RecipeMappingConfiguration : IRegister
             .Map(dest => dest.CreatorId, src => src.CreatorId.Value)
             .Map(dest => dest.ReviewIds, src => src.ReviewIds.Select(id => id.Value).ToList());
         
+        config.NewConfig<Recipe, GetRecipeResponse>()
+            .Map(dest => dest.Id, src => src.Id.Value)
+            .Map(dest => dest.CreatorId, src => src.CreatorId.Value)
+            .Map(dest => dest.ReviewIds, src => src.ReviewIds.Select(id => id.Value).ToList());
+        
         config.NewConfig<CookingDirection, CookingDirectionResponse>()
             .Map(dest => dest.Id, src => src.Id.Value);
         
         config.NewConfig<Ingredient, IngredientResponse>()
             .Map(dest => dest.Id, src => src.Id.Value);
-
-
+        
         config.NewConfig<(Guid recipeId, Guid? creatorId), DeleteRecipeCommand>()
             .Map(dest => dest.RecipeId, src => RecipeId.Create(src.recipeId))
             .Map(dest => dest.CreatorId, src => UserId.Create(src.creatorId!.Value));
