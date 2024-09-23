b32442ddf2e64d2397c92617efb2a00801851b90
skmkqw <tkorsakov77@gmail.com>
2024-09-23 20:30:44 +0200

    GetRecipeResponse

---
+++ src/Foodieland.Contracts/Recipes/Common/GetRecipeResponse.cs
@@ -0,0 +1,20 @@
+namespace Foodieland.Contracts.Recipes.Common;
+
+public record GetRecipeResponse(
+    string Id,
+    string Name,
+    string Description,
+    int TimeToCook,
+    string CreatorId,
+    NutritionInformationResponse NutritionInformation,
+    List<CookingDirectionResponse> Directions,
+    List<IngredientResponse> Ingredients,
+    List<string> ReviewIds,
+    DateTime CreatedDateTime,
+    DateTime UpdatedDateTime);
+
+public record NutritionInformationResponse(int Calories, float Protein, float Carbs, float Fat);
+
+public record CookingDirectionResponse(string Id, int StepNumber, string Name, string Description);
+
+public record IngredientResponse(string Id, string Name, float Quantity, string Unit);
