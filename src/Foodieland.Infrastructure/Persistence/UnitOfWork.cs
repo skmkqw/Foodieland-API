99818e3a4ebde08887063ed5eeaf34f8d9c8401d
skmkqw <tkorsakov77@gmail.com>
2024-09-25 20:34:26 +0200

    UpdateTimestamps method

--- src/Foodieland.Infrastructure/Persistence/UnitOfWork.cs
+++ src/Foodieland.Infrastructure/Persistence/UnitOfWork.cs
@@ -1,4 +1,7 @@
 using Foodieland.Application.Common.Interfaces.Persistence;
+using Foodieland.Domain.Common.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace Foodieland.Infrastructure.Persistence;
 
@@ -13,6 +16,30 @@ public class UnitOfWork : IUnitOfWork
 
     public async Task SaveChangesAsync(CancellationToken cancellationToken = default)
     {
+        UpdateTimestamps();
+        
         await _dbContext.SaveChangesAsync(cancellationToken);
+    }
+
+    private void UpdateTimestamps()
+    {
+        IEnumerable<EntityEntry<IHasTimestamps>> entries = _dbContext
+            .ChangeTracker
+            .Entries<IHasTimestamps>();
+
+        foreach (EntityEntry<IHasTimestamps> entityEntry in entries)
+        {
+            if (entityEntry.State == EntityState.Added)
+            {
+                entityEntry.Property(x => x.CreatedDateTime)
+                    .CurrentValue = DateTime.UtcNow;
+            }
+
+            if (entityEntry.State == EntityState.Modified)
+            {
+                entityEntry.Property(x => x.UpdatedDateTime)
+                    .CurrentValue = DateTime.UtcNow;
+            }
+        }
     }
 }
