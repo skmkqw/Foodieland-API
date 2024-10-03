00bb859652712021a7bcbae41bb5e0811491ced7
skmkqw <tkorsakov77@gmail.com>
2024-10-03 09:26:32 +0200

    User.NotFound & User.Unauthorized errors

--- src/Foodieland.Domain/Common/Errors/Errors.User.cs
+++ src/Foodieland.Domain/Common/Errors/Errors.User.cs
@@ -9,5 +9,13 @@ public static partial class Errors
         public static Error DuplicateEmail => Error.Conflict(
             code: "User.DuplicateEmail", 
             description: "A user with given email already exists");
+        
+        public static Error NotFound => Error.NotFound(
+            code: "User.NotFound",
+            description: "User not found or doesn't exist");
+        
+        public static Error Unauthorized => Error.Unauthorized(
+            code: "User.Unauthorized",
+            description: "You are not authorized to modify this account");
     }
 }
