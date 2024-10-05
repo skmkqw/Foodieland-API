1105e1d33e217b172785bc81eaebdbac6af643f9
skmkqw <tkorsakov77@gmail.com>
2024-10-05 21:03:00 +0200

    ReviewsController & CreateReview endpoint

---
+++ src/Foodieland.API/Controllers/ReviewsController.cs
@@ -0,0 +1,40 @@
+using Foodieland.Application.Reviews.Commands;
+using Foodieland.Contracts.Reviews.CreateReview;
+using MapsterMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Foodieland.API.Controllers;
+
+[Route("reviews")]
+public class ReviewsController : ApiController
+{
+    private readonly IMapper _mapper;
+    
+    private readonly IMediator _mediator;
+
+    public ReviewsController(IMapper mapper, IMediator mediator)
+    {
+        _mapper = mapper;
+        _mediator = mediator;
+    }
+
+
+    [HttpPost("recipe/{recipeId}")]
+    public async Task<IActionResult> CreateReview([FromRoute] Guid recipeId, [FromBody] CreateReviewRequest request)
+    {
+        var userId = GetUserId();
+        if (!userId.HasValue)
+        {
+            return UnauthorizedUserIdProblem();
+        }
+        
+        var command = _mapper.Map<CreateReviewCommand>((recipeId, userId, request));
+        
+        var createReviewResult = await _mediator.Send(command);
+        
+        return createReviewResult.Match(
+            onValue: recipe => Ok(_mapper.Map<CreateReviewResponse>(recipe)),
+            onError: errors => Problem(errors));
+    }
+}
