d6cfcde8b80973469f4ec7c0ba617f05f349a1a3
skmkqw <tkorsakov77@gmail.com>
2024-10-07 20:03:56 +0200

    GetRecipeReviewsQuery

---
+++ src/Foodieland.Application/Reviews/Queries/GetRecipeReviews/GetRecipeReviewsQuery.cs
@@ -0,0 +1,9 @@
+using ErrorOr;
+using Foodieland.Application.Common.Models;
+using Foodieland.Domain.RecipeAggregate.ValueObjects;
+using Foodieland.Domain.ReviewAggregate;
+using MediatR;
+
+namespace Foodieland.Application.Reviews.Queries.GetRecipeReviews;
+
+public record GetRecipeReviewsQuery(RecipeId RecipeId, int Page, int PageSize) : IRequest<ErrorOr<PagedResult<Review>>>;
