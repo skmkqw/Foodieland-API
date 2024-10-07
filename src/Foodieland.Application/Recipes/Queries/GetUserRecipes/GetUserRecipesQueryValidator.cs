be275e51fb7a21b4a6486e5483c33fc92ed8c9f8
skmkqw <tkorsakov77@gmail.com>
2024-10-07 20:55:52 +0200

    updated IPagedQuery validators

--- src/Foodieland.Application/Recipes/Queries/GetUserRecipes/GetUserRecipesQueryValidator.cs
+++ src/Foodieland.Application/Recipes/Queries/GetUserRecipes/GetUserRecipesQueryValidator.cs
@@ -1,20 +1,12 @@
 using FluentValidation;
+using Foodieland.Application.Common.Validators;
 
 namespace Foodieland.Application.Recipes.Queries.GetUserRecipes;
 
-public class GetUserRecipesQueryValidator : AbstractValidator<GetUserRecipesQuery>
+public class GetUserRecipesQueryValidator : PagedQueryValidator<GetUserRecipesQuery>
 {
     public GetUserRecipesQueryValidator()
     {
         RuleFor(x => x.UserId).NotEmpty().WithMessage("User ID cannot be empty");
-        
-        RuleFor(x => x.Page)
-            .NotEmpty().WithMessage("Page number cannot be empty")
-            .GreaterThanOrEqualTo(1).WithMessage("Page number must be greater than or equal to 1");
-        
-        RuleFor(x => x.PageSize)
-            .NotEmpty().WithMessage("Page size cannot be empty")
-            .GreaterThanOrEqualTo(1).WithMessage("Page size must be greater than or equal to 1")
-            .LessThanOrEqualTo(10).WithMessage("Page size must be less than or equal to 10");
     }
 }
