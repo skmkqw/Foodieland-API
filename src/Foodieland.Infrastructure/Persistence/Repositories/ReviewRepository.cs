b8948efea9614cfd3ede04ddf67ed8d95b8f92f2
skmkqw <tkorsakov77@gmail.com>
2024-10-07 18:42:16 +0200

    GetUserReviewForRecipe repository method

--- src/Foodieland.Infrastructure/Persistence/Repositories/ReviewRepository.cs
+++ src/Foodieland.Infrastructure/Persistence/Repositories/ReviewRepository.cs
@@ -1,5 +1,7 @@
 using Foodieland.Application.Common.Interfaces.Persistence;
+using Foodieland.Domain.RecipeAggregate.ValueObjects;
 using Foodieland.Domain.ReviewAggregate;
+using Foodieland.Domain.UserAggregate.ValueObjects;
 
 namespace Foodieland.Infrastructure.Persistence.Repositories;
 
@@ -15,5 +17,10 @@ public class ReviewRepository : IReviewRepository
     public void AddReview(Review review)
     {
         _dbContext.Reviews.Add(review);
+    }
+
+    public Review? GetUserReviewForRecipe(RecipeId recipeId, UserId userId)
+    {
+        return _dbContext.Reviews.FirstOrDefault(r => r.RecipeId == recipeId && r.CreatorId == userId);
     }
 }
