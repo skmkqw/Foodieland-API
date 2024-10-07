7132312eb2e73224a4e4c694e3b97aaa76062bf6
skmkqw <tkorsakov77@gmail.com>
2024-10-07 20:31:05 +0200

    new mapping configs

--- src/Foodieland.API/Mapping/AuthenticationMappingConfiguration.cs
+++ src/Foodieland.API/Mapping/AuthenticationMappingConfiguration.cs
@@ -1,7 +1,14 @@
 using Foodieland.Application.Authentication.Commands.Register;
 using Foodieland.Application.Authentication.Common;
 using Foodieland.Application.Authentication.Queries.Login;
+using Foodieland.Application.Common.Models;
 using Foodieland.Contracts.Authentication;
+using Foodieland.Contracts.Common;
+using Foodieland.Contracts.Recipes.GetRecipes;
+using Foodieland.Contracts.Reviews.GetReview;
+using Foodieland.Contracts.Reviews.GetReviews;
+using Foodieland.Domain.RecipeAggregate.ValueObjects;
+using Foodieland.Domain.ReviewAggregate;
 using Mapster;
 using RegisterRequest = Microsoft.AspNetCore.Identity.Data.RegisterRequest;
 
@@ -18,5 +25,17 @@ public class AuthenticationMappingConfiguration : IRegister
         config.NewConfig<AuthenticationResult, AuthenticationResponse>()
             .Map(dest => dest.Id, src => src.User.Id.Value)
             .Map(dest => dest, src => src.User);
+
+        config.NewConfig<Review, GetReviewResponse>()
+            .Map(dest => dest.Id, src => src.Id.Value)
+            .Map(dest => dest.RecipeId, src => RecipeId.Create(src.RecipeId.Value))
+            .Map(dest => dest.CreatorId, src => src.CreatorId.Value);
+        
+        config.NewConfig<PagedResult<Review>, GetReviewsResponse>()
+            .Map(dest => dest.Reviews, src => src.Items)
+            .Map(dest => dest.Pagination, src => new Pagination(
+                src.Page, 
+                src.PageSize, 
+                src.TotalCount));
     }
 }
