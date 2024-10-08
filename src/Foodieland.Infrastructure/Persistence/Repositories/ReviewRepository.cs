d1a5c439396e9debf31fb45db2bd77b934895f89
skmkqw <tkorsakov77@gmail.com>
2024-10-08 14:03:33 +0200

    GetUserReviews repository method

--- src/Foodieland.Infrastructure/Persistence/Repositories/ReviewRepository.cs
+++ src/Foodieland.Infrastructure/Persistence/Repositories/ReviewRepository.cs
@@ -33,6 +33,18 @@ public class ReviewRepository : IReviewRepository
         return new PagedResult<Review>(recipeReviews, totalReviews, page, pageSize);
     }
 
+    public async Task<PagedResult<Review>> GetUserReviews(UserId userId, int page, int pageSize)
+    {
+        var totalReviews = _dbContext.Reviews.Count(r => r.CreatorId == userId);
+        
+        var recipeReviews = await _dbContext.Reviews.Where(r => r.CreatorId == userId)
+            .Skip((page - 1) * pageSize)
+            .Take(pageSize)
+            .ToListAsync();
+        
+        return new PagedResult<Review>(recipeReviews, totalReviews, page, pageSize);
+    }
+
     public async Task<Review?> GetUserReviewForRecipe(RecipeId recipeId, UserId userId)
     {
         return await _dbContext.Reviews.FirstOrDefaultAsync(r => r.RecipeId == recipeId && r.CreatorId == userId);
