c253ab3d5e4965e8a4dd57de084e89117dd8f2fc
skmkqw <tkorsakov77@gmail.com>
2024-10-12 23:19:46 +0200

    Errors.Review.NotFound & Unauthorized domain errors

--- src/Foodieland.Domain/Common/Errors/Errors.Review.cs
+++ src/Foodieland.Domain/Common/Errors/Errors.Review.cs
@@ -6,6 +6,14 @@ public static partial class Errors
 {
     public static class Review
     {
+        public static Error NotFound => Error.NotFound(
+            code: "Review.NotFound",
+            description: "Review not found or doesn't exist");
+        
+        public static Error Unauthorized => Error.Unauthorized(
+            code: "Review.Unauthorized",
+            description: "You are not authorized to modify this review");
+        
         public static Error DuplicateReview => Error.Conflict(
             code: "Review.Conflict",
             description: "This recipe has already been reviewed by this user");
